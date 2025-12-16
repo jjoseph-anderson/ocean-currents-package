07c37b635588013782d8735232793e5edda4698d
Joseph Anderson <josephanderson7180@gmail.com>
2025-12-16 21:10:31 +0000

    added NSP plotting

--- ocean_currents/plotting.py
+++ ocean_currents/plotting.py
@@ -1,5 +1,6 @@
 # plotting.py
 import matplotlib.pyplot as plt
+import numpy as np
 
 class Plotter:
     def __init__(self):
@@ -12,3 +13,20 @@ class Plotter:
         plt.legend()
         plt.grid(True)
         plt.show()
+
+    def plot_NSP(self, k, Ux_raw, Uy_raw, rot_angle):
+        Ux_transformed_NSP = np.cos(rot_angle) * Ux_raw - np.sin(rot_angle) * Uy_raw
+        Uy_transformed_NSP = np.sin(rot_angle) * Ux_raw + np.cos(rot_angle) * Uy_raw
+
+        plt.scatter(k, np.abs(Ux_transformed_NSP), label='$U_x$', color='blue')
+        plt.scatter(k, np.abs(Uy_transformed_NSP), label='$U_y$', color='red')
+        plt.grid(True)
+        plt.legend(loc='upper right')
+        plt.ylabel("U [m/s]")
+        plt.xlabel("k [rad/m]")
+        plt.title("Rotated NSP Doppler Shifts")
+        plt.ylim(-0.05, 1.6)
+        plt.xlim(0, 0.37)
+        plt.show()
+
+
--- usage_example.py
+++ usage_example.py
@@ -1,23 +1,24 @@
 from ocean_currents.data_loader import DataLoader
 from ocean_currents.plotting import Plotter
+import numpy as np
 
-# Load data
+# specify a radar image sequence and a current direction
 date = "20"
 hour = "00"
 direction = "North"
+
+# Required Parameters
+rot_angle = 4*np.pi/9 # rotation angle for nsp points
+
 NSP_data_path = rf"C:\Users\josep\Desktop\PHYC40900_Project TP\Post_Review_Images\Generalize Process\NSP Points\NSP_{date}_{hour}.txt"
 ADCP_data_path = rf"C:\Users\josep\Desktop\PHYC40900_Project TP\Post_Review_Images\Generalize Process\ADCP measurments\{date}Jan2022_{hour}00.mat"
 
-
-
+# Load Data
 loader = DataLoader(NSP_data_path, ADCP_data_path)
 U_east, U_north, z = loader.load_adcp()
+k_NSP, Ux_NSP, Uy_NSP = loader.load_nsp()
 
-# Fit PEDM
-# pedm = PEDM(water_depth=16)
-# p = pedm.fit(k, Ux)
-# U_fun = lambda z: pedm.evaluate(p, z)
-#
 # # Plot
 plotter = Plotter()
-plotter.plot_profiles(z, U_east)
+#plotter.plot_profiles(z, U_east)
+plotter.plot_NSP(k_NSP, Ux_NSP, Uy_NSP, rot_angle)
